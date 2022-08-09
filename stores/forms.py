985a335dfcca8420b3907025306ede06fcac1a97
Mansour Alh <mansouralhussain94@gmail.com>
2022-08-09 19:38:38 +0300

    create_store_item

---
+++ stores/forms.py
@@ -0,0 +1,8 @@
+from pyexpat import model
+from django import forms
+from .models import StoreItem
+
+class StoreItemForm (forms.ModelForm):
+    class Meta :
+        model=StoreItem
+        fields =["name", "description", "kind", "price"]
